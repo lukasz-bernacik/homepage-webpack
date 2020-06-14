11a5a756fca8737f010f8e979d9b26bec15e8903
Łukasz Bernacik <lukasz.bernacik@gmail.com>
2020-06-14 12:40:32 +0200

    move files from hompage repository to hompage-webpack

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,21 @@ import '../scss/main.scss';
 
 /* place your code below */
 
+const hamburger = document.querySelector('.navigation__hamburger--js');
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.menu--js')
+    nav.classList.toggle('menu--open');
+})
+
+const name = 'Łukasz';
+const age = 31;
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+createContent ('.hero__subheading--js', `Hej! Mam na imię ${name} i mam ${age} lat`);
+
 console.log('HELLO 🚀')
